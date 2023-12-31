034e90db4610233920e8a7d44df2012b0364382e
andrevitormb <andrevitoreur@gmail.com>
2023-12-31 15:22:17 +0100

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import { Transactions } from "./pages/Transactions";
+import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
+      <GlobalStyle/>
+      <Transactions/>
     </ThemeProvider>
   )
 }
